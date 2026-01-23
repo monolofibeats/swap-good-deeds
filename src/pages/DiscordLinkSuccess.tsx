6bf0630251c7448eba279e15cc9bccfd5da18b2e
monolofibeats <mono.lofibeats@gmail.com>
2026-01-23 20:44:15 +0100

    Update DiscordLinkSuccess.tsx

--- src/pages/DiscordLinkSuccess.tsx
+++ src/pages/DiscordLinkSuccess.tsx
@@ -6,7 +6,7 @@ export default function DiscordLinkSuccess() {
 
   return (
     <div style={{ padding: 24, maxWidth: 720, margin: "0 auto" }}>
-      <h1 style={{ fontSize: 28, fontWeight: 700 }}>Discord connected ✅</h1>
+      <h1 style={{ fontSize: 28, fontWeight: 700 }}>Discord connected</h1>
       <p style={{ marginTop: 12, opacity: 0.9 }}>
         {username ? (
           <>Connected as <b>{username}</b>. You can close this tab.</>
